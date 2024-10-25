4320116d7ec9a2dea576326c18ead5b4fd995365
MO HUAIHAO <mohuaihaoxm@gmail.com>
2024-10-25 15:21:59 -0400

    Week 3

---
+++ Week 3: Linked Lists & Matrices/palindrome-linked-list.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode head2 = reverseList(mid);
+        while (head2 != null) {
+            if (head.val != head2.val) { // 不是回文链表
+                return false;
+            }
+            head = head.next;
+            head2 = head2.next;
+        }
+        return true;
+    }
+
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+}
